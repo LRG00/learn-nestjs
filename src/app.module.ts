bd21d330aad18acb459df81f86df4b65c23bb03e
LRG00 <158097628@qq.com>
2019-09-25 17:22:54 +0800

    连接mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule, EventsModule],
+  imports: [
+    CatsModule,
+    EventsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '120.77.239.216',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'abc',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
